e067cdbc32a2287ff6176337830afc45444b0e33
Thomas Woods <>
2018-12-07 20:57:57 -0500

    some fixes and setting up favorites

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,8 @@
 
 	function displayMovieInfo(){
 		$("#theDiv").empty();
-		$('.more').remove()
+		$('.more').remove();
+		$('.my-stats').remove()
 		theOffset = 0;
 		var queryURL = "https://api.giphy.com/v1/gifs/search?q="+$(this).attr("data-name")+"&api_key=dc6zaTOxFJmzC&limit=10";
 		var theItem = $(this).attr("data-name");
@@ -12,7 +13,6 @@
 			method: "GET"
 		}).then(function(response) {
 			for (var key in response.data) {
-				console.log(key, response.data[key]);
 				var theImageHTML = response.data[key].images.original_still.url;
 				var theImage =$("<img>");
 				theImage.attr( "src", theImageHTML);
@@ -34,19 +34,43 @@
 				var theImageHTML = $(this).attr("data-still");
 				$(this).attr( "src", theImageHTML);        
 			});
+			$(".imageHover").on("click", function(event) {
+				
+				
+/*
+				var theImage =$("<img>");
+				theImage.attr( "src", $(this).attr("data-still"));
+				theImage.attr("data-still", $(this).attr("data-still"));        
+				theImage.attr("data-gif", $(this).attr("data-gif"));        
+				theImage.addClass("imageHover");
+				theImage.height(200);    
+				var myDiv = $("<div>");
+				myDiv.addClass("my-div");
+				myDiv.append(theImage);
+				$(".my-favorites").append(myDiv);
+				
+				$(".imageHover").on("mouseover", function(){
+					var theImageHTML = $(this).attr("data-gif");
+					$(this).attr( "src", theImageHTML);        
+				});
+				$(".imageHover").on("mouseout", function(){
+					var theImageHTML = $(this).attr("data-still");
+					$(this).attr( "src", theImageHTML);        
+				});
+*/
+				
+				
+				
+			});
 			var a = $("<button>");
 			a.addClass("more");
 			a.addClass("btn btn-primary");
 			a.attr("data-name", $(this));
 			a.text("Load More");
 			$(".col-lg-3").append(a);
-			console.log(theItem);
 			$(".more").on("click", function(event) {
 				theOffset +=10;
 				loadMore(theItem);
-			});
-			$(".imageHover").on("click", function(event) {
-				console.log("poop");
 			});
 		});
 		
@@ -55,12 +79,19 @@
 		$.ajax({
 			url: queryURL,
 			method: "GET"
-		}).then(function(response) {
-			console.log("Name: "+capital_letter(response.name));
-			console.log("Number: "+response.id);
+		}).then(function(response) {			
+			var myDiv = $("<div>");
+				myDiv.addClass("my-stats");
+				myDiv.append("<p>Name: " + capital_letter(response.name) + "</p>");
+				myDiv.append("<p>Number: "+response.id + "</p>");
+			var theStats = $("<p>");
 			for (var key in response.stats) {
-				console.log(response.stats[key].stat.name+": "+response.stats[key].base_stat);
+				theStats.prepend("<p>"+capital_letter(response.stats[key].stat.name)+": "+response.stats[key].base_stat+"</p>");
 			};
+			
+			myDiv.append(theStats);
+			
+			$(".my-stats-div").append(myDiv);
 		});
 		
 		
@@ -84,7 +115,6 @@
 			method: "GET"
 		}).then(function(response) {
 			for (var key in response.data) {
-				console.log(key, response.data[key]);
 				var theImageHTML = response.data[key].images.original_still.url;
 				var theImage =$("<img>");
 				theImage.attr( "src", theImageHTML);
@@ -105,6 +135,35 @@
 			$(".imageHover").on("mouseout", function(){
 				var theImageHTML = $(this).attr("data-still");
 				$(this).attr( "src", theImageHTML);        
+			});
+			$(".imageHover").on("click", function(event) {
+				
+				
+/*
+				var theImage =$("<img>");
+				theImage.attr( "src", $(this).attr("data-still"));
+				theImage.attr("data-still", $(this).attr("data-still"));        
+				theImage.attr("data-gif", $(this).attr("data-gif"));        
+				theImage.addClass("imageHover");
+				theImage.height(200);    
+				var myDiv = $("<div>");
+				myDiv.addClass("my-div");
+				myDiv.append(theImage);
+				$(".my-favorites").append(myDiv);
+				
+				$(".imageHover").on("mouseover", function(){
+					var theImageHTML = $(this).attr("data-gif");
+					$(this).attr( "src", theImageHTML);        
+				});
+				$(".imageHover").on("mouseout", function(){
+					var theImageHTML = $(this).attr("data-still");
+					$(this).attr( "src", theImageHTML);        
+				});
+*/
+
+				
+				
+				
 			});
 		});
 	}
